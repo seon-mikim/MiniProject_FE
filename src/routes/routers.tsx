a94dbaf239075c3b59be4a2b1414df050ed8d9ad
Nevacat <nevacattery217@naver.com>
2023-05-02 17:30:05 +0900

    Docs: 프로젝트 기본 셋팅 수정 및 에러페이지 추가

--- src/routes/routers.tsx
+++ src/routes/routers.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Layout from '../components/common/Layout';
-import LoginPage from '../pages/LoginPage';
-import RegisterPage from '../pages/RegisterPage';
-import AdminApprovalPage from '../pages/AdminApprovalPage';
-import AdminAuthPage from '../pages/AdminAuthPage';
-import Mypage from '../pages/MyPage';
-import MainPage from '../pages/MainPage';
+import LoginPage from '../pages/LoginPage/LoginPage';
+import RegisterPage from '../pages/RegisterPage/RegisterPage';
+import AdminApprovalPage from '../pages/AdminApprovalPage/AdminApprovalPage';
+import AdminAuthPage from '../pages/AdminAuthPage/AdminAuthPage';
+import Mypage from '../pages/MyPage/MyPage';
+import MainPage from '../pages/MainPage/MainPage';
+import ErrorPage from '../pages/ErrorPage/ErrorPage';
 
 function Routers() {
   return (
@@ -18,6 +19,7 @@ function Routers() {
         <Route path="/mypage" element={<Mypage/>} />
         <Route path="/adminApproval" element={<AdminApprovalPage/>} />
         <Route path="/adminAuth" element={<AdminAuthPage/>} />
+        <Route path="*" element={<ErrorPage/>}/>
       </Route>
     </Routes>
   );
