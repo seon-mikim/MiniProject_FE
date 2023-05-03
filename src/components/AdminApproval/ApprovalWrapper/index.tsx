5cf39cfb2ec44e832a0a3d0bae379d02f75c4fb8
seon-mikim <tjsalsun@gmail.com>
2023-05-03 21:49:51 +0900

    📜fe t: 컨포넌트 화를 위한 기능구현

---
+++ src/components/AdminApproval/ApprovalWrapper/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './style'
+
+export default function ApprovalWrapper() {
+  return (
+    <S.Wrapper>
+      
+    </S.Wrapper>
+  )
+}
---
+++ src/components/AdminApproval/ApprovalWrapper/style.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Wrapper = styled.div`
+
+  width: 1308px;
+  height: 882px;
+  border-radius: 8px;
+  background-color: ${(props) => props.theme.color.beige};
+`;
