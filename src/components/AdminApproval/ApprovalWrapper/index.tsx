211559a03d48077fc9a101dee44c70bf25896f86
seon-mikim <tjsalsun@gmail.com>
2023-05-04 15:44:23 +0900

    remove: 불필요한 파일 삭제

--- src/components/AdminApproval/ApprovalWrapper/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as S from './style'
-
-export default function ApprovalWrapper() {
-  return (
-    <S.Wrapper>
-      
-    </S.Wrapper>
-  )
-}
--- src/components/AdminApproval/ApprovalWrapper/style.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Wrapper = styled.div`
-
-  width: 1308px;
-  height: 882px;
-  border-radius: 8px;
-  background-color: ${(props) => props.theme.color.beige};
-`;
