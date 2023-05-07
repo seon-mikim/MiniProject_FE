0350e74413b86aff9fd779e7815971c184950bc4
김민기 <kimminki.dev@gmail.com>
2023-05-08 05:24:52 +0900

    feat: nav메뉴 관리자 부분 내용 화면 표시

--- src/components/common/Layout/Nav/Menu/Admin.tsx
+++ src/components/common/Layout/Nav/Menu/Admin.tsx
@@ -1,8 +1,31 @@
 import * as S from './style'
+import { Link } from 'react-router-dom'
 
 function Admin() {
+  const holidayCount = 5
+  const nightCount = 10
+
   return (
-    <div>Admin</div>
+    <div>
+      <div>
+        <Link to='/adminApproval'>
+          <span>연차 승인 대기</span>
+          <span>{holidayCount}</span>
+        </Link>
+      </div>
+      <div>
+        <Link to='/adminApproval'>
+          <span>당직 승인 대기</span>
+          <span>{nightCount}</span>
+        </Link>
+      </div>
+      <span>
+        <Link to='/adminAuth'>관리자 권한 설정</Link>
+      </span>
+      {/* <span>
+        <Link to=''>계정 승인 설정</Link>
+      </span> */}
+    </div>
   )
 }
 
