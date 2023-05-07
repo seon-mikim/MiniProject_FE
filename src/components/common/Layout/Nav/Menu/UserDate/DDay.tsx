5ad1f4f1f690d3727fc458bc0bda5d789d60856d
김민기 <kimminki.dev@gmail.com>
2023-05-08 04:38:42 +0900

    feat: D-Day 계산 컴포넌트 기능 완성

--- src/components/common/Layout/Nav/Menu/UserDate/DDay.tsx
+++ src/components/common/Layout/Nav/Menu/UserDate/DDay.tsx
@@ -1,8 +1,15 @@
 import * as S from './style'
 
-function DDay() {
+function DDay({eventDay}: {eventDay: Date}) {
+  const today = new Date();
+  const todayMs = today.getTime()
+  const eventDayMs = eventDay.getTime()
+  const dDayMs = eventDayMs - todayMs
+
+  const dDay = Math.floor(dDayMs / (24 * 60 * 60 * 1000))
+  
   return (
-    <div>DDay</div>
+    <span>D-{dDay === 0 ? 'Day' : dDay < 10 ? '0' + dDay : dDay}</span>
   )
 }
 
