95420a05e0d8d1efdf4a77c5b3cc8b6ea7ebc604
김민기 <kimminki.dev@gmail.com>
2023-05-08 05:23:05 +0900

    feat: nav메뉴 유저 부분 내용 화면 표시

--- src/components/common/Layout/Nav/Menu/User.tsx
+++ src/components/common/Layout/Nav/Menu/User.tsx
@@ -1,8 +1,31 @@
 import * as S from './style'
+import DateType from './UserDate/DateType';
+import DDay from './UserDate/DDay';
+import { Link } from 'react-router-dom';
 
 function User() {
+  const holiday = new Date('2023-05-20')
+  const night = new Date('2023-05-18')
+
   return (
-    <div>User</div>
+    <div>
+      <div>
+        <span>연차</span>
+        <DDay eventDay={holiday}/>
+        <DateType eventDay={holiday}/>
+      </div>
+      <div>
+        <span>당직</span>
+        <DDay eventDay={night}/>
+        <DateType eventDay={night}/>
+      </div>
+      <span>
+        <Link to='/main'>연차/당직 신청</Link>
+      </span>
+      <span>
+        <Link to='/mypage'>마이 페이지</Link>
+      </span>
+    </div>
   )
 }
 
