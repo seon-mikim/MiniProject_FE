5a14105f79cbf5d91023d748ebc51356895eac0b
김민기 <kimminki.dev@gmail.com>
2023-05-08 04:38:59 +0900

    feat: DateType 컴포넌트 기능 완성

--- src/components/common/Layout/Nav/Menu/UserDate/DateType.tsx
+++ src/components/common/Layout/Nav/Menu/UserDate/DateType.tsx
@@ -1,8 +1,15 @@
 import * as S from './style'
 
-function DateType() {
+function DateType({eventDay}: {eventDay: Date}) {
+  const year = eventDay.getFullYear()
+  const month = eventDay.getMonth() + 1
+  const date = eventDay.getDate()
+  const eventDayStr = String(year) + '.' +
+    (month<10 ? '0'+String(month): String(month)) + '.' +
+    (date<10 ? '0'+String(date): String(date))
+
   return (
-    <div>Date</div>
+    <span>{eventDayStr}</span>
   )
 }
 
