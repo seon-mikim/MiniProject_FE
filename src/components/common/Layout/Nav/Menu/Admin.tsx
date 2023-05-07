b5411b322673800ef69b877188610a42c5c11cfd
김민기 <kimminki.dev@gmail.com>
2023-05-08 01:10:28 +0900

    conf: nav의 사용자, 관리자 메뉴 구분을 위한 컴포넌트 생성

---
+++ src/components/common/Layout/Nav/Menu/Admin.tsx
@@ -0,0 +1,9 @@
+import * as S from './style'
+
+function Admin() {
+  return (
+    <div>Admin</div>
+  )
+}
+
+export default Admin
---
+++ src/components/common/Layout/Nav/Menu/User.tsx
@@ -0,0 +1,9 @@
+import * as S from './style'
+
+function User() {
+  return (
+    <div>User</div>
+  )
+}
+
+export default User
