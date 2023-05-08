d8445fc26afc95288cc51dd7a6eb93726b12844f
Nevacat <nevacattery217@naver.com>
2023-05-08 23:49:08 +0900

    feat: 당직/연차 리스트 card 컴포넌트 생성

---
+++ src/components/MainForm/AnnualSection/AnnualCard/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import * as S from './style'
+
+function AnnualList() {
+
+
+
+  return (
+    <S.List>
+      <S.UserImg/>
+    </S.List>
+  )
+}
+
+export default AnnualList
