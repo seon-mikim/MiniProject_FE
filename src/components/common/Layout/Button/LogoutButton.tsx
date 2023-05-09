506d7ee782a23185acb8e95380696f74c48c6bd8
김민기 <kimminki.dev@gmail.com>
2023-05-09 15:49:42 +0900

    conf: 로그아웃 버튼 컴포넌트 생성

---
+++ src/components/common/Layout/Button/LogoutButton.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Button } from '../../Button/style'
+
+function LogoutButton() {
+  return (
+    <>
+      <button>로그아웃</button>
+    </>
+  )
+}
+
+export default LogoutButton
