96ad0f1b85883c6abf73dafbe5a627b68a6da9ab
김민기 <kimminki.dev@gmail.com>
2023-05-10 21:42:33 +0900

    feat: header의 profileSection에 grid 레이아웃 적용

--- src/components/common/Layout/Header/ProfileSection/index.tsx
+++ src/components/common/Layout/Header/ProfileSection/index.tsx
@@ -4,13 +4,12 @@ import { User, UserHeaderProfile } from "../../../../../interface/User"
 function ProfileSection({userData}: {userData: UserHeaderProfile}) {
   console.log(userData)
 
-
   return (
-    <div>
+    <S.gridDiv>
       <span>{userData.username}</span>
       <span>{userData.email}</span>
       <div><img src={userData.email}/></div>
-    </div>
+    </S.gridDiv>
   )
 }
 
--- src/components/common/Layout/Header/ProfileSection/style.ts
+++ src/components/common/Layout/Header/ProfileSection/style.ts
@@ -1,2 +1,9 @@
 import theme from "../../../../../styles/theme";
 import styled from "styled-components";
+
+export const gridDiv = styled.div`
+  display: grid;
+  grid-template-columns: 2fr 1fr;
+  grid-template-rows: 1fr 1fr;
+  width: 10rem;
+`
