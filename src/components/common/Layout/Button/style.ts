ab8427ed9bea9e26d89587d5d37fdafaa4722b95
김민기 <kimminki.dev@gmail.com>
2023-05-12 01:35:52 +0900

    fix: 버튼 테두리 제거, 그림자 스타일 적용

--- src/components/common/Layout/Button/style.ts
+++ src/components/common/Layout/Button/style.ts
@@ -9,10 +9,12 @@ export const logoutButton = styled.button<opacityProps>`
   background-color: ${theme.color.lightBrown};
   color: ${theme.color.white};
   border-radius: 10px;
+  border: none;
   padding: 0.5rem;
   width: 100%;
   display: ${(props) => props.opacity === 0 ? 'none' : 'block'};
   font-weight: ${theme.fontWeight.bold};
+  box-shadow: 1px 1px 3px 3px rgba(0, 0, 0, 0.2);
   cursor: pointer;
   &:hover {
     opacity: 0.9;
@@ -25,8 +27,10 @@ export const mypageButton = styled.button<opacityProps>`
   border-radius: 10px;
   padding: 0.5rem;
   width: 100%;
+  border: none;
   display: ${(props) => props.opacity === 0 ? 'none' : 'block'};
   font-weight: ${theme.fontWeight.bold};
+  box-shadow: 1px 1px 3px 3px rgba(0, 0, 0, 0.3);
   cursor: pointer;
   &:hover {
     opacity: 0.9;
