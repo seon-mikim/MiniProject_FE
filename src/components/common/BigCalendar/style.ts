c4f42ee1498da22507b814b92c7d55d7878896c2
Nevacat <nevacattery217@naver.com>
2023-05-12 23:46:24 +0900

    feat: BigCalendar로직 구현 (아직 구현중..)

---
+++ src/components/common/BigCalendar/style.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const BigCalendarContainer = styled.div`
+  .rbc-event,
+  .rbc-event-continues-after {
+    color:#000;
+    background-color: #fff;
+  }
+`
