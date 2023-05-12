8577f8afdf8dd04181e96bf945d22ff7f851ec9e
Nevacat <nevacattery217@naver.com>
2023-05-12 23:47:47 +0900

    feat: 연차/당직 리스트 섹션 로직 구현

--- src/components/MainForm/AnnualSection/index.tsx
+++ src/components/MainForm/AnnualSection/index.tsx
@@ -1,39 +1,75 @@
 import AnnualList from './AnnualCard';
+import BigCalendar from '../../common/BigCalendar';
 import * as S from './style';
-
+import { useState } from 'react';
+import moment from 'moment';
+import { useQuery } from 'react-query';
+import { getEventList } from '../../../api/mainService';
+import ListItem from '../../common/ListItem';
 
 function AnnualSection() {
-  const today = new Date(); 
-
-  function formatDate(date : Date) {
-    const year = date.getFullYear();
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const day = String(date.getDate()).padStart(2, '0');
-    return `${year}. ${month}. ${day}`;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [type, setType] = useState('ANNUAL');
+  const today = moment();
+  const { data } = useQuery(['eventList', type, today.format('YYYY-MM')], () =>
+    getEventList(type, today.format('YYYY-MM')),
+  );
+  function handleModalOpen() {
+    setIsModalOpen(true);
+  }
+  function handleModalClose(e: React.MouseEvent<HTMLDivElement>) {
+    if (e.target === e.currentTarget) setIsModalOpen(false);
   }
 
+  const onClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+    const { innerText } = e.target as HTMLButtonElement;
+    setType(innerText === '연차' ? 'ANNUAL' : 'DUTY');
+  };
+
   return (
-    <S.Section>
-      <S.SectionHeader>
-        <S.H2>Today: {formatDate(today)}</S.H2>
-        <S.HeaderRight>
-          <S.H2>당직 / 연차 현황</S.H2>
-          <S.CalendarIcon/>
-        </S.HeaderRight>
-      </S.SectionHeader>
-      <S.ButtonWrap>
-        <S.NavButton className='active'>연차</S.NavButton>
-        <S.NavButton>당직</S.NavButton>
-      </S.ButtonWrap>
-      <S.ListWrap>
-        <AnnualList/>
-        <AnnualList/>
-        <AnnualList/>
-        <AnnualList/>
-        <AnnualList/>
-        <AnnualList/>
-      </S.ListWrap>
-    </S.Section>
+    <>
+      {isModalOpen && (
+        <S.ModalBackground onClick={handleModalClose}>
+          <BigCalendar />
+        </S.ModalBackground>
+      )}
+      <S.Section>
+        <S.SectionHeader>
+          <S.H2>TODAY: {today.format('YYYY.MM.DD')}</S.H2>
+          <S.HeaderRight>
+            <S.H2>당직 / 연차 현황</S.H2>
+            <S.CalendarIcon onClick={handleModalOpen} />
+          </S.HeaderRight>
+        </S.SectionHeader>
+        <S.ButtonWrap>
+          <S.NavButton onClick={onClick} className={type === 'ANNUAL' ? 'active' : ''}>
+            연차
+          </S.NavButton>
+          <S.NavButton onClick={onClick} className={type === 'DUTY' ? 'active' : ''}>
+            당직
+          </S.NavButton>
+        </S.ButtonWrap>
+        <S.ListWrap >
+          {data && data.length !== 0 ? (
+            data.map((item: any) => (
+              <ListItem
+                key={item.eventId}
+                imageUri={item.userImageUri ? item.userImageUri :'./default_profile.png'}
+                username={item.userName}
+                email={item.userEmail}
+                textContent={[
+                  <p>
+                    {item.startDate} ~ {item.endDate}
+                  </p>,
+                ]}
+              />
+            ))
+          ) : (
+            <p>일정이 없습니다.</p>
+          )}
+        </S.ListWrap>
+      </S.Section>
+    </>
   );
 }
 
--- src/components/MainForm/AnnualSection/style.ts
+++ src/components/MainForm/AnnualSection/style.ts
@@ -3,6 +3,7 @@ import styled from "styled-components";
 
 export const Section = styled.div`
   box-sizing:border-box;
+  min-width:540px;
   width:780px;
   height:720px;
   background-color: ${({ theme }) => theme.color.beige};
@@ -57,4 +58,44 @@ export const ListWrap = styled.ul`
   height:552px;
   padding: 0 30px;
   overflow: auto;
+  font-size: ${({ theme }) => theme.fontSize.base};
+  font-weight: ${({ theme }) => theme.fontWeight.bold};
+  & > * {
+    margin-bottom: 20px;
+  }
+  &::-webkit-scrollbar {
+    width: 5px;
+    background-color: ${({ theme }) => theme.color.lightBeige};
+  }
+  &::-webkit-scrollbar-thumb {
+    background-color: ${({ theme }) => theme.color.lightBrown};
+    border-radius: 5px;
+  }
 `
+
+export const ModalBackground = styled.div`
+  position:absolute;
+  top:0;
+  left:0;
+  width:100%;
+  height:100vh;
+  backdrop-filter:blur(3px);
+  z-index:90;
+  .rbc-calendar{
+    position: absolute;
+    background-color: #CAB3A3;
+    z-index: 99;
+    top:55px;
+    left:55px;
+    right:0;
+    bottom:0;
+    margin:auto;
+    border-radius: 8px;
+    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    padding: 20px;
+  }
+  .rbc-toolbar-label{
+    font-size: ${({ theme }) => theme.fontSize.large};
+    font-weight: ${({ theme }) => theme.fontWeight.bold};
+  }
+  `
