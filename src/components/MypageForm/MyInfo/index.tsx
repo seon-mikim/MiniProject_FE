d9ed21afc4173cf212aa885fa74c1ddd110fe60d
김민기 <kimminki.dev@gmail.com>
2023-05-14 05:28:15 +0900

    feat: 마이페이지 회원정보 수정 폼 작성
    - mypage api 연결
    - 폼 작성 및 유효성 검사 작성

--- src/components/MypageForm/MyInfo/index.tsx
+++ src/components/MypageForm/MyInfo/index.tsx
@@ -1,8 +1,128 @@
+import { useState, useEffect, useRef } from 'react'
+import { User } from '../../../interface/User'
 import * as S from './style'
+import { useForm } from 'react-hook-form'
 
-function MyInfo() {
+interface mypageForm {
+  username: string,
+  phone: string,
+  password: string,
+  passwordCheck: string,
+  imageChange?: object | string
+}
+
+function MyInfo({username, email, phone, imageUri, role}: 
+  {username: string | undefined, email: string | undefined, phone: string | undefined, imageUri: string | undefined, role: string | undefined}) {
+  
+  const { register, watch, handleSubmit, resetField, formState: { errors} } = useForm({
+    defaultValues: {
+      username: username,
+      phone: phone,
+      password: '',
+      passwordCheck: '',
+      imageChange: ''
+    }
+  })
+
+  const src = useRef<any>()
+  src.current = watch('imageChange')[0]
+
+  const onValid = (data: any) => console.log(data, 'onValid')
+  const onInvalid = (data: any) => console.log(data, 'onInvalid')
+
+  const isImage = (filetype: string) => {
+    const form = /(.*?)\.(jpg|jpeg|gif|bmp|png)$/
+    if(!filetype.match(form)){
+      window.alert('이미지 파일만 업로드 가능합니다!')
+      resetField('imageChange')
+    }
+  }
+
+  const password = useRef<any>()
+  password.current = watch('password')
+  
   return (
-    <div>MyInfo</div>
+    <div>
+      <span>My Info</span>
+      <form onSubmit={handleSubmit(onValid, onInvalid)}>
+        <div>
+          <img src={src.current ? URL.createObjectURL(src.current) : imageUri} />
+          <label htmlFor='imageChange'>이미지 변경</label>
+          <input type='file' 
+            accept='image/*' 
+            id='imageChange'
+            {...register('imageChange', 
+              {
+                onChange: (e: any) => isImage(e.target.value),
+              }
+              )}/>
+        </div>
+        <div>
+          <div>
+            <span>직급</span>
+            <span>{role === 'USER' ? '사원':'관리자'}</span>
+          </div>
+          <div>
+            <span>이메일</span><span>{email}</span>
+          </div>
+          <div>
+            <label>이름</label>
+            <input {...register('username', {
+              required: true,
+              minLength: 2
+            })} />
+            {errors.username && errors.username.type === 'required'
+              && <p>이름을 입력해주세요!</p>}
+            {errors.username && errors.username.type === 'minLength'
+              && <p>2자 이상 입력해주세요!</p>}
+          </div>
+          <div>
+            <label>연락처</label>
+            <input placeholder='010-****-****'{...register('phone', {
+              required: 'true',
+              pattern: /^010-\d{4}-\d{4}$/
+            })} />
+            {errors.phone && errors.phone.type === 'required'
+              && <p>연락처를 입력해주세요!</p>}
+            {errors.phone && errors.phone.type === 'pattern'
+              && <p>010-****-**** 형식으로 입력해주세요!</p>}
+          </div>
+          <div>
+            <label>비밀번호</label>
+            <input type='password' placeholder='문자, 숫자, 기호를 모두 포함 8자 이상'
+            {...register('password', {
+              required: 'true',
+              minLength: 8,
+              pattern: /^(?=.*[A-Za-z])(?=.*\d)(?=.*\W)(?!.*\s).{8,}$/
+            })} />
+            {errors.password && errors.password.type === 'required'
+              && <p>비밀번호를 입력해주세요!</p>}
+            {errors.password && errors.password.type === 'minLength'
+              && <p>8자 이상 입력해주세요!</p>}
+            {errors.password && errors.password.type === 'pattern'
+              && <p>문자, 숫자, 기호를 모두 포함하여 입력해주세요!</p>}
+          </div>
+          <div>
+            <label>비밀번호 확인</label>
+            <input type='password' {...register('passwordCheck', {
+              required: 'true',
+              minLength: 8,
+              pattern: /^(?=.*[A-Za-z])(?=.*\d)(?=.*\W)(?!.*\s).{8,}$/,
+              validate: (value) => value === password.current
+            })} />
+            {errors.passwordCheck && errors.passwordCheck.type === 'required'
+              && <p>비밀번호를 한번 더 입력해주세요!</p>}
+            {errors.passwordCheck && errors.passwordCheck.type === 'minLength'
+              && <p>8자 이상 입력해주세요!</p>}
+            {errors.passwordCheck && errors.passwordCheck.type === 'pattern'
+              && <p>문자, 숫자, 기호를 모두 포함하여 입력해주세요!</p>}
+            {errors.passwordCheck && errors.passwordCheck.type === 'validate'
+              && <p>비밀번호가 일치하지 않습니다!</p>}
+          </div>
+        </div>  
+        <button type='submit'>정보 수정</button>
+      </form>
+    </div>
   )
 }
 
--- src/components/MypageForm/index.tsx
+++ src/components/MypageForm/index.tsx
@@ -1,14 +1,27 @@
+import { useState } from 'react'
 import MyList from './MyList'
 import MyInfo from './MyInfo'
+import { User, UserMyInfo } from '../../interface/User'
+import { getMyInfo } from '../../api/mypage'
+import { useQuery } from 'react-query'
 
-function index() {
+function MypageForm() {
+  const { data: myinfo, isLoading, error } = useQuery(['myinfo'], getMyInfo)
+
+  if(isLoading) return <>로딩 중...</>
+  if(error) return <>error</>
+
   return (
     <>
-      <MyInfo />
+      <MyInfo username={myinfo?.username}
+        email={myinfo?.email}
+        phone={myinfo?.phone}
+        imageUri={myinfo?.imageUri}
+        role={myinfo?.role} />
       <MyList title={'나의 연차내역'}/>
       <MyList title={'나의 당직내역'}/>
     </>  
   )
 }
 
-export default index
+export default MypageForm
