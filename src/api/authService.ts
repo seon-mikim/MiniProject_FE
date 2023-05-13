3c728c4e66458cc780d1665342e16d06f4f279ae
jyj1111 <jyj202040@gmail.com>
2023-05-13 23:09:59 +0900

    📜feat: 로그인, 회원가입 toast 에러 알람창 구현

--- src/api/authService.ts
+++ src/api/authService.ts
@@ -3,14 +3,18 @@ import { LoginResponse, RegisterResponse, LoginRequest, RegisterRequest } from '
 import { axiosFormInstance, axiosJsonInstance } from './axios';
 import { setCookie } from '../utils/cookies';
 
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 export const login = async (user: LoginRequest) => {
   try {
     const { data, headers } = await axiosJsonInstance.post<LoginResponse>('/api/login', user);
     headers.authorization && setCookie('accessToken', headers.authorization.split(' ')[1]);
     return data;
   } catch (error) {
-    if (error instanceof AxiosError && error.response?.status === 400) {
-      alert('이메일 또는 비밀번호가 틀렸습니다.');
+    if (error instanceof AxiosError && error.response?.data.status === 401) {
+      toast.error('아이디 또는 비밀번호를 확인하세요', { position: toast.POSITION.TOP_CENTER, autoClose: 3000 });
+      //alert('이메일 또는 비밀번호가 틀렸습니다.');
     } else {
       console.error(error);
     }
@@ -28,8 +32,9 @@ export const register = async (user: RegisterRequest) => {
     const { data } = await axiosFormInstance.post<RegisterResponse>('/api/signup', formData);
     return data;
   } catch (error) {
-    if (error instanceof AxiosError && error.response?.status === 400) {
-      alert('이메일 또는 유저이름이 중복되었습니다.');
+    if (error instanceof AxiosError && error.response?.data.status === 401) {
+      toast.error('이메일 또는 유저이름이 중복되었습니다.', { position: toast.POSITION.TOP_CENTER, autoClose: 3000 });
+      //alert('이메일 또는 유저이름이 중복되었습니다.');
     } else {
       console.error(error);
     }
