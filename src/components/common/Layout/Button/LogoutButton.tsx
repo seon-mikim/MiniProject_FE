4bca74e8f029a813901fc3db4eb89e8da0e15b73
김민기 <kimminki.dev@gmail.com>
2023-05-15 00:33:19 +0900

    feat: 로그아웃 버튼 기능 연결

--- src/components/common/Layout/Button/LogoutButton.tsx
+++ src/components/common/Layout/Button/LogoutButton.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import { useCookies } from 'react-cookie'
+import { useNavigate } from 'react-router-dom'
 import * as S from './style'
 
 function LogoutButton({opacity}: {opacity: number}) {
+  const [, , removeCookie ] = useCookies(['accessToken'])
+  const navigate = useNavigate()
+  const handleClick = () => {
+    removeCookie('accessToken')
+    navigate('/')
+  }
   return (
-      <S.logoutButton className='logout' opacity={opacity}>로그아웃</S.logoutButton>
+      <S.logoutButton onClick={handleClick} className='logout' opacity={opacity}>로그아웃</S.logoutButton>
   )
 }
 
