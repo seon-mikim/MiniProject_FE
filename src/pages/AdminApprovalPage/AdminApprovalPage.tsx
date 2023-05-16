5d67930d5034ef46ec6160806c3e21e5b7b50663
seon-mikim <tjsalsun@gmail.com>
2023-05-16 13:34:39 +0900

    refatory : 수정

--- src/pages/AdminApprovalPage/AdminApprovalPage.tsx
+++ src/pages/AdminApprovalPage/AdminApprovalPage.tsx
@@ -1,4 +1,4 @@
-import { useState,useEffect } from 'react';
+import { useState } from 'react';
 import Wrapper from '../../components/AdminApproval';
 import { useMutation, useQuery, useQueryClient } from 'react-query';
 import { getRequestList, postApproval } from '../../api/request';
